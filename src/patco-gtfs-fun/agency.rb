ad8781d775ca8ff26dd40af57ba28d23ed746b49
Ariel Kirkwood <ariel.kirkwood@me.com>
2013-06-05 00:47:27 -0400

    baby steps towards a good initalize()

--- src/patco-gtfs-fun/agency.rb
+++ src/patco-gtfs-fun/agency.rb
@@ -1,3 +1,6 @@
-class Agency < FieldDefinition
-
+class Agency < CSV::Row
+  # attr_accessor :headers, :fields (maybe need this in the future?)
+  def initialize(headers = ["agency_name", "agency_url", "agency_timezone"], fields)
+    super(headers, fields)
+  end
 end
