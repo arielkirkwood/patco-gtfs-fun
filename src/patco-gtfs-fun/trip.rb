0f87c003050ddc49ca5bb6965cd2885ec4b42ac2
Ariel Kirkwood <ariel.kirkwood@me.com>
2013-06-16 19:36:12 -0400

    added trip_headsign and direction_id to trip class for better quality data on trips

--- src/patco-gtfs-fun/trip.rb
+++ src/patco-gtfs-fun/trip.rb
@@ -3,7 +3,7 @@
 class Trip < CSV::Row
 
   # Function used to instantiate this class.
-  def initialize(headers = [:route_id, :service_id, :trip_id], fields)
+  def initialize(headers = [:route_id, :service_id, :trip_id, :trip_headsign, :direction_id], fields)
     # Here, we delegate to the CSV::Row initialize() because we want its goodies included as well.
     super(headers, fields) 
   end
